b949d28e24762e920f6236c324bb42dee04036bb
Amanda Godsey <adgodsey@gmail.com>
2021-01-25 15:14:43 -0800

    Truthy Falsy

---
+++ Javascript/Truthy_Falsy/app.js
@@ -0,0 +1,28 @@
+//Falsy values: false, 0, ""(empty string), null, undefined, NaN
+//Everything else is truthy!
+
+// const userInput = prompt("enter something");
+// If nothing entered for prompt, empty string is userInput
+// if(userInput) {
+//     console.log("Truthy")
+// } else {
+//     console.log("Falsy")
+// }
+
+// if(0) {
+//     console.log("Truthy")
+// } else {
+//     console.log("Falsy")
+// }
+
+if(null) {
+    console.log("Truthy")
+} else {
+    console.log("Falsy")
+}
+
+// if('hahaha') {
+//     console.log("Truthy")
+// } else {
+//     console.log("Falsy")
+// }
