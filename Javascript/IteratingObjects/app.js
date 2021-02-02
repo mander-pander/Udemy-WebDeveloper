bfaa3988c15245f1b99a183525a3b0c4462b1e9f
Amanda Godsey <adgodsey@gmail.com>
2021-02-01 21:24:43 -0800

    Iterating Objects

---
+++ Javascript/IteratingObjects/app.js
@@ -0,0 +1,27 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+
+// for(let person in testScores) {
+//     console.log(`${person} scored ${testScores[person]}`);
+// }
+
+//method of Object.keys(testScores)
+//Object.values(testScores)
+//Object.entries(testScores) 
+
+let total = 0;
+let scores = Object.values(testScores);
+for(let score of scores) {
+    total += score;
+}
+console.log(total / scores.length);
