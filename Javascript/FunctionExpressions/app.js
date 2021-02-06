e53e7794ecb8404919a6e77867d607137758bad1
Amanda Godsey <adgodsey@gmail.com>
2021-02-05 17:11:52 -0800

    Function Expressions

---
+++ Javascript/FunctionExpressions/app.js
@@ -0,0 +1,13 @@
+//function statement
+// function add(x,y){
+//     return x + y;
+// }
+
+//fuction expression 
+const add = function (x,y) {
+    return x + y;
+}
+
+const square = function (x) {
+    return x*x;
+}
