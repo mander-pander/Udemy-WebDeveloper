dff18a96ded8404b2879b2ff0f4604e12a2cb59f
Amanda Godsey <adgodsey@gmail.com>
2021-02-17 20:45:40 -0800

    Default Parameters

---
+++ Javascript/DefaultParam/app.js
@@ -0,0 +1,20 @@
+// function rollDie(numSides) {
+//     if (numSides === undefined) {
+//         numSides = 6
+//     }
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
+// function rollDie(numSides = 6) {
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
+// function greet(msg, person) {
+//     console.log (`${msg}, ${person}!`)
+// }
+
+// function greet(msg = "Hey there", person = "pal") {
+//     console.log (`${msg}, ${person}!`)
+// }
+
+
