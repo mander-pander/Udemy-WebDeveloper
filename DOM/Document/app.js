691465437ab3a76f9aa1c789c9d59413e43c3a5f
Amanda Godsey <adgodsey@gmail.com>
2021-02-18 15:48:35 -0800

    DOM - Document

---
+++ DOM/Document/app.js
@@ -0,0 +1,17 @@
+//document.getElementById("adlfjsldkjf");
+//document.getElementsByTagName('sadlkjfsdf');
+//document.getElementsByClassName('alsdkfjsladf);
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let img of allImages) {
+//     console.log(img.src)
+// }
+
+// const squareImages = document.getElementsByClassName('square');
+
+// for(let img of squareImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Male_Silkie.png/460px-Male_Silkie.png'
+// }
+
+//
