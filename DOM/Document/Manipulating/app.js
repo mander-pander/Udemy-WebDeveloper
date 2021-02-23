65b24788177dee7845d1db363a27e87d505dfa64
Amanda Godsey <adgodsey@gmail.com>
2021-02-22 18:12:39 -0800

    Manipulating

---
+++ DOM/Document/Manipulating/app.js
@@ -0,0 +1,27 @@
+const allLinks = document.querySelectorAll('a');
+
+// for(let link of allLinks) {
+//     link.innerText = 'I AM A LINK!'
+// }; //changes all hyperlinks to say I AM A LINK!
+
+//.innerHTML retrieves full contents including tag name
+//.innerText just grabs the actual text in what is showing and ignores elements
+
+// document.querySelector('span').innerText = "Disgusting";
+
+//document.querySelector('a').title = "List of chicken breeds"
+
+// const firstLink = document.querySelector('a');
+// firstLink.href //file.///wiki/List_of_chicken_breeds
+// //this is going to javascript object
+
+// firstLink.getAttribute('href') // /wiki/List_of_chicken_breeds
+// //gets it directly from html
+
+// firstLink.setAttribute('href', 'http://www.google.com')
+
+// const input = document.querySelector('input[type ="text"]');
+//input.type = 'password'
+//input.type = 'color'
+//input.setAttribute('type', 'text')
+
