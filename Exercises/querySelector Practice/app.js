298bd16deee76e6c3cb5769fe92666a3cbe12101
Amanda Godsey <adgodsey@gmail.com>
2021-02-24 07:51:02 -0800

    querySelector Practice

---
+++ Exercises/querySelector Practice/app.js
@@ -0,0 +1,5 @@
+//Select all elements that have the class of "done".
+const doneTodos = document.querySelectorAll(".done");
+
+//Select the one checkbox. Be aware of multiiple input elements.
+const checkbox = document.querySelector('input[type="checkbox"]');
