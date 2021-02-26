8add07dd4c842cbbfa12ac42c6cd26d0bd3ea62d
Amanda Godsey <adgodsey@gmail.com>
2021-02-25 21:47:55 -0800

    Manipulating

--- DOM/Document/Manipulating/app.js
+++ DOM/Document/Manipulating/app.js
@@ -25,3 +25,43 @@ const allLinks = document.querySelectorAll('a');
 //input.type = 'color'
 //input.setAttribute('type', 'text')
 
+for(let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy';
+}
+
+//const h2 = document.querySelector('h2);
+//h2.getAttribute('class');
+// h2.setAttribute('class', 'purple')
+//classList access the class attributes of an element
+//h2.classList.add('purple')
+//h2.classList.add('border')
+//h2.classList.remove('purple')
+//h2.classList.contains('purple') --false
+//classList.toggles
+
+
+//.parent returns parent element, only one direct parent
+//.children, can be multiple (.children[0], .children[1])
+//.childElementCount returns how many
+//const = squareImg = .document.querySelector('.square');
+//.nextSibling & .previousSibling return node
+//.nextElementSibling returns actual next sibling
+
+const newH3 = document.createElement('h3');
+newH3.innerText = 'I am a new H3!';
+document.body.appendChild(newH3);
+
+//.insertAdjacentElement(position, element);
+const h2 = document.createElement('h2');
+h2.append("are kinda cute");
+const h1 = document.querySelector('h1');
+h1.insertAdjacentElement('afterend', h2)
+
+const firstLi = document.querySelector('li');
+const ul = firstLi.parentElement;
+ul.removeChild(firstLi);
+
+const img = document.querySelector('img');
+img.remove();
