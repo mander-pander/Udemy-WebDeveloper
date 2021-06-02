eea8f427c8c7f6cd0aefd78a926b5cd517dd3e58
Amanda Godsey <adgodsey@gmail.com>
2021-06-02 15:51:38 -0700

    daily commit

--- Mongo_Relationships/DemoApp/index.js
+++ Mongo_Relationships/DemoApp/index.js
@@ -25,8 +25,42 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 //Farm Routes
+
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms })
+});
+
 app.get('/farms/new', (req, res) => {
     res.render('farms/new')
+});
+
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm })
+});
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+});
+
+app.get('/farms/:id/products/new', (req, res) => {
+    const { id } = req.params;
+    res.render('products/new', { categories, id })
+});
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm)
 })
 
 //Product Routes
--- Mongo_Relationships/DemoApp/models/product.js
+++ Mongo_Relationships/DemoApp/models/product.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const productSchema = new mongoose.Schema({
+
+const productSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -13,10 +15,14 @@ const productSchema = new mongoose.Schema({
     category: {
         type: String,
         lowercase: true,
-        enum: ['fruit', 'vegetable', 'dairy',]
+        enum: ['fruit', 'vegetable', 'dairy']
+    },
+    farm: {
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
     }
-})
+});
 
 const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product;
+module.exports = Product; 
